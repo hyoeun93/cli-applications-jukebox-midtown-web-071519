c2f93c8ca4ffa046eb859ada60e26386c3da1ac7
HyoEun Lee <hyoeun93@gmail.com>
2019-07-11 18:57:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  inputs = gets.chomp 
+  inputs = gets.downcase.chomp 
   
   if (1..9).to_a.include?(user_response.to_i)
     puts "Playing #{songs[user_response.to_i - 1]}"
